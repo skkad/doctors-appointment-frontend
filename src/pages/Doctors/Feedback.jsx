312e2fb03415a4967a7143ad6158301e93093872
Kadir Shaikh <skkad1998@gmail.com>
2024-07-16 00:33:49 +0530

    fix: all changes of frontend

--- src/pages/Doctors/Feedback.jsx
+++ src/pages/Doctors/Feedback.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import avatar from "../../assets/images/avatar-icon.png";
 import { formateDate } from "../../utils/formateDate";
 import { AiFillStar } from "react-icons/ai";
 import FeedbackForm from "./FeedbackForm";
